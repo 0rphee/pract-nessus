e2e51b9b09fd7c2d6fe54e77e8b35fba59da7fed
0rphee <0rph3e@proton.me>
2024-11-01 13:02:10 -0600

    fix host so that server is accesible beyond vm

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ def search():
     return render_template_string(html_template, results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
